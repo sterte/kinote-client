4109a074d714757df9eb35c0ac2577f9329358ac
sterte@gmail.com <sterte@gmail.com>
2020-11-11 10:31:42 +0100

    Initial components added

--- kinote/src/app/app.module.ts
+++ kinote/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PlanComponent } from './plan/plan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule
